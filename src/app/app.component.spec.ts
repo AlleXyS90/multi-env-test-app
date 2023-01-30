40fcab0c7c563f314a20581fcc0f39c8019eeef9
Alexandru Tudor <alexandru-adrian.tudor@endava.com>
2023-01-30 11:20:54 +0200

    app targeting to es5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { APP_CONFIG } from './app.module';
+import { TestService } from './services/test-service';
 
 describe('AppComponent', () => {
   let fixture;
